925feb1b4a9a56f0aae965cc93daea7786cd86cc
DubbyTT <dubbytt@gmail.com>
2012-08-31 14:14:34 -0500

    Ooops... needle2.png

--- blue_style.js
+++ blue_style.js
@@ -52,7 +52,7 @@ function blueStyle() {
 	$("#KhSjI").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat");
 
     //needle
-	$("#vote_needle").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/needle.png') 0 0 no-repeat");
+	$("#vote_needle").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/needle2.png') 0 0 no-repeat");
 
 	//Mute Button
 	$("a.mute_btn").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/mute_btn2.png') 0 0 no-repeat");
