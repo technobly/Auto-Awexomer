0cc0c249c0c1e29f58a448dad420fad049704640
DubbyTT <dubbytt@gmail.com>
2012-08-31 13:52:16 -0500

    Updated Vote Buttons

--- blue_style.js
+++ blue_style.js
@@ -58,9 +58,10 @@ function turnTheTable() {
     //$("#right_speaker").css("visibility", "hidden");
 	$("#right_speaker").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/right_speaker2.png') 0 0 no-repeat");
 
-	//Vote Buttons
-	$("#VMosQiJNkOo").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat");
+	//Vote Up Button
+	$("#VMosQiJNkOo").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat").css("background-position","0 -90px");
 
+	//Vote Down Button
 	$("#KhSjI").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat");
 
     //needle
