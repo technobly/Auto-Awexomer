6b86643b7a8d834fee88cf3c45aecf8d59cfa51a
DubbyTT <dubbytt@gmail.com>
2012-08-31 23:52:53 -0500

    Merged Blue Style with Auto Awexomer...

--- auto_awexome.js
+++ auto_awexome.js
@@ -8,6 +8,67 @@
  * find me in http://turntable.fm/code_ninjas
  */
 
+// Auto Awexomer
+// -----------------------------------------------------
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+  script = document.createElement( 'script' );
+  script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+	script.onload=blueStyle;
+	document.body.appendChild(script);
+}
+else {
+	blueStyle();
+}
+
+function blueStyle() {
+	//alert("B^Dub's Blue Style Enable");
+	$("#turntable").children(":first-child").children(":nth-child(2)").children().each(function(i){
+		switch(i){
+			case 2:
+				//background
+				$(this).attr("src", "https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/wallpaper2.png");
+			break;
+			case 3:
+				//floor
+				$(this).attr("src", "https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/floor2.png");
+			break;
+			case 4:
+				//table
+				$(this).attr("src", "https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/dj_table2.png");
+			break;
+			case 5:
+				//gauge
+				$(this).attr("src", "https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/gauge2.png");
+			break;
+		}
+	});
+
+	//song board
+	$("#songboard").css("color", "cyan");
+	$("#songboard_artist").css("text-shadow", "none"); // 40px default, has a red shadow by default, removing it makes it crisp
+	$("#songboard_title").css("text-shadow", "none").css("font-size","18px"); // 15px default, has a red shadow by default, removing it makes it better... still hard to read because it's small font size
+
+	//left speaker
+	$("#left_speaker").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/left_speaker2.png') 0 0 no-repeat");
+
+	//right speaker
+    $("#right_speaker").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/right_speaker2.png') 0 0 no-repeat");
+
+	//Vote Up Button
+	$("#VMosQiJNkOo").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat").css("background-position","0 -90px");
+
+	//Vote Down Button
+	$("#KhSjI").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat");
+
+    //Meter Needle
+	$("#meterNeedle").children(":first-child").children(":first-child").attr("src", "https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/needle2.png");
+
+	//Mute Button
+	$("a.mute_btn").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/mute_btn2.png') 0 0 no-repeat");
+}
+
+// Auto Awexomer
+// -----------------------------------------------------
 $(document).ready(function() {
   if(window.bdub == undefined) window.bdub = { };
   
@@ -165,7 +226,7 @@ $(document).ready(function() {
         else
           window.bdub.showArc = false;
 
-        window.bdub.botMessage = $('<div id="bot-message">B^Dub\'s Auto Awexome. <span style="font-style: italic;"></span> <a href="#" style="text-decoration: none; color: yellow; font-weight: bold;">Turn off</a></div>');
+        window.bdub.botMessage = $('<div id="bot-message">B^Dub\'s Auto Awexomer. <span style="font-style: italic;"></span> <a href="#" style="text-decoration: none; color: yellow; font-weight: bold;">Turn off</a></div>');
         window.bdub.botMessage.css({
           position: 'fixed',
           color: 'white',
