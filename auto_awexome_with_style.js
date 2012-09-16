8ee29d9a7913cf11ccde565a6acf355b44f4c781
DubbyTT <dubbytt@gmail.com>
2012-09-16 08:19:56 -0500

    Double Style Apply Prevention Added

--- auto_awexome_with_style.js
+++ auto_awexome_with_style.js
@@ -71,11 +71,13 @@ function blueStyle() {
 	}
 }
 
+
+//if(typeof double_click_check === "undefined") var double_click_check = true; //allow styles to be applied only once
+if(double_click_check) {
 // Auto Awexomer
 // -----------------------------------------------------
 $(document).ready(function() {
-  if(typeof double_click_check === "undefined") var double_click_check = true; //allow styles to be applied only once
-  if(double_click_check) {
+  
   if(window.bdub == undefined) window.bdub = { };
   
   window.bdub = $.extend(window.bdub, {
@@ -298,6 +300,7 @@ $(document).ready(function() {
   });
   
   window.bdub.init();
-  double_click_check = false; //make sure a second click doesn't re-apply the styles
-  }
+  
 });
+  double_click_check = false; //make sure a second click doesn't re-apply the styles
+}
