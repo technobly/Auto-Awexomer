75f9c57e98795e0556ac40553d6be0f5ddc39052
DubbyTT <dubbytt@gmail.com>
2012-09-17 18:49:15 -0500

    Updated Button Selectors

--- blue_style.js
+++ blue_style.js
@@ -56,7 +56,7 @@ function blueStyle() {
 	//$("#hwdSSRfEep").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat");
 	
 	//Vote Up Button
-	$("#cRtzMXltLmflC").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat").css("background-position","0 -90px");
+	$("#LeQzXMMaP").css("background", "url('https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/vote_btns2.png') 0 0 no-repeat").css("background-position","0 -90px");
 
 	//Meter Needle
 	$("#meterNeedle").children(":first-child").children(":first-child").attr("src", "https://raw.github.com/DubbyTT/Auto-Awexomer/master/images/needle2.png");
